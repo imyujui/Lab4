16e1375ec8c878d93e4073dc32800df1edd90f6a
imyujui <162032284+imyujui@users.noreply.github.com>
2024-06-08 23:22:42 +0800

    Create JPA401.java

---
+++ JPA401.java
@@ -0,0 +1,9 @@
+public class JPA107 {
+    public static void main(String[] args){
+        int action = 1 ;
+        int skill = 2;
+        int teamgame = 3;
+        System.out.println("The basketball grade is " + (action + skill + teamgame));
+        System.out.printf("The baseball grade is " + (10 + skill + teamgame));
+    }
+}
